b12c4a5e86d446352d6337c9a6fd34cef4ed4d7a
Matt Billings <mbillings@gmail.com>
2020-10-01 16:19:47 -0700

    play random vox

--- js/instruments/vox.ts
+++ js/instruments/vox.ts
@@ -4,14 +4,18 @@ import vox1 from '../../static/samples/vox_C.mp3';
 import vox2 from '../../static/samples/vox_G2.mp3';
 import vox3 from '../../static/samples/vox_G3.mp3';
 
+const playRandomVox = () => {
+  const players = [vox1Player, vox2Player, vox3Player];
+  const randomInt = Math.floor(Math.random() * Math.floor(players.length));
+  players[randomInt].start();
+};
+
 const vox1Pan = new Panner(-1).toDestination();
 
 const vox1Player = new Player({
   url: vox1,
   volume: -16,
-  onstop: () => {
-    vox2Player.start();
-  },
+  onstop: playRandomVox,
 }).connect(vox1Pan);
 
 const vox2Pan = new Panner(1).toDestination();
@@ -19,17 +23,13 @@ const vox2Pan = new Panner(1).toDestination();
 const vox2Player = new Player({
   url: vox2,
   volume: -16,
-  onstop: () => {
-    vox3Player.start();
-  },
+  onstop: playRandomVox,
 }).connect(vox2Pan);
 
 const vox3Player = new Player({
   url: vox3,
   volume: -16,
-  onstop: () => {
-    vox1Player.start();
-  },
+  onstop: () => playRandomVox,
 }).toDestination();
 
 export const startVox = () => {
