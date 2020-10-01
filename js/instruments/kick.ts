f6c68651c7989f64fa39c687ee2778801edc6d78
Matt Billings <mbillings@gmail.com>
2020-10-01 08:09:49 -0700

    decent kick sound

--- js/instruments/kick.ts
+++ js/instruments/kick.ts
@@ -1,3 +1,14 @@
-import { MembraneSynth } from 'tone';
+import { MembraneSynth, Loop } from 'tone';
 
-export const kickInst = new MembraneSynth().toDestination();
+export const kickInst = new MembraneSynth({
+  envelope: {
+    attack: 0.1,
+    decay: 0.2,
+  },
+}).toDestination();
+
+export const loopKick = () => {
+  new Loop((time) => {
+    kickInst.triggerAttackRelease('E1', time);
+  }, '2n').start(0);
+};
--- js/main.ts
+++ js/main.ts
@@ -1,6 +1,17 @@
 import * as Tone from 'tone';
 import 'minireset.css';
-import '../css/styles.css';
-import { kickInst } from './instruments/kick';
 
-kickInst.triggerAttackRelease('C2', '8n');
+import '../css/styles.css';
+import { loopKick } from './instruments/kick';
+
+// constants
+const BPM = 100;
+
+const initInst = () => {
+  Tone.Transport.start();
+  loopKick();
+};
+
+Tone.Transport.bpm.value = BPM;
+Tone.start();
+initInst();
